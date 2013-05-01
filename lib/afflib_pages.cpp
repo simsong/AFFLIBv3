726984832dc4d261294d30818704a9dd5f0aa780
John Lindgren <john.lindgren@aol.com>
2013-05-01 16:16:34 -0400

    Optimize is_buffer_zero().

--- lib/afflib_pages.cpp
+++ lib/afflib_pages.cpp
@@ -314,9 +314,35 @@ int af_get_page(AFFILE *af,int64_t pagenum,unsigned char *data,size_t *bytes)
 
 static bool is_buffer_zero(unsigned char *buf,int buflen)
 {
-    for(int i=0;i<buflen;i++){
-	if(buf[i]) return false;
+    if(buflen >= (int)sizeof(long))
+    {
+        // align to word boundary
+        buflen -= (intptr_t)buf % sizeof(long);
+
+        while((intptr_t)buf % sizeof(long))
+        {
+            if(*buf++)
+                return false;
+        }
+
+        // read in words
+        long *ptr = (long*)buf;
+        buf += buflen - buflen % sizeof(long);
+        buflen %= sizeof(long);
+
+        while(ptr < (long*)buf)
+        {
+            if(*ptr++)
+                return false;
+        }
     }
+
+    while(buflen--)
+    {
+        if(*buf++)
+            return false;
+    }
+
     return true;
 }
 
